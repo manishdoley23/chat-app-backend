3dde7f3ea9f9a08f943049a5dac977ec2065aa32
manishdoley23 <manishdoley23@gmail.com>
2024-03-23 18:42:38 +0530

    init

--- dist/routes/get-users.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUsers = void 0;
-var getUsers = function (req, res, next) {
-    res.send({
-        Hello: "World",
-    });
-};
-exports.getUsers = getUsers;
--- dist/routes/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var get_users_1 = require("./get-users");
-var router = (0, express_1.Router)();
-router.get("/", get_users_1.getUsers);
-exports.default = router;
